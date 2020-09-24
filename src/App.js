7c14ceea84a072c14d6c06e1664b58a298243320
ali raza <aliahmadraza401@gmail.com>
2020-09-24 09:32:37 +0500

    dskasjdf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Haloooo Raza  How are u
         </p>
         <a
           className="App-link"
